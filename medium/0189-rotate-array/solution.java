ba03bf3779a0a872cb07adb258ea6350ce77804c
Aditya Rai <145740878+Adityaraitechguy@users.noreply.github.com>
2026-08-17 22:08:52 +0530

    ✅ Rotate Array | Medium | Java | Runtime: 0 ms | Memory: 43 MB | 2026-08-17

--- medium/0189-rotate-array/solution.java
+++ medium/0189-rotate-array/solution.java
@@ -1,18 +1,20 @@
-        int n = nums.length;
-        k %= n; // handle k > n
-        
-        reverse(nums, 0, n - 1);   // reverse whole array
-        reverse(nums, 0, k - 1);   // reverse first k elements
-        reverse(nums, k, n - 1);   // reverse the rest
-    }
+        int n=nums.length;
+    public void rotate(int[] nums, int k) {
+class Solution {
+        k=k%n;
+        reverse(nums,0,n-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,n-1);
     
-    private void reverse(int[] nums, int left, int right) {
-        while (left < right) {
-            int temp = nums[left];
+              
+    }
+    private void reverse(int nums[],int left,int right){
+        while(right>left){
+            int temp= nums[left];
             nums[left] = nums[right];
-            nums[right] = temp;
+            nums[right]= temp;
             left++;
-            right--;
+            right--; 
         }
     }
-}
+
