9a6205a1c33209fcb5fb87925f0bedc83fe57871
Aditya Rai <145740878+Adityaraitechguy@users.noreply.github.com>
2026-08-17 22:54:06 +0530

    ✅ Move Zeroes | Easy | Java | Runtime: 2 ms (91.98%) | Memory: 47.5 MB | 2026-08-17

--- easy/0283-move-zeroes/solution.java
+++ easy/0283-move-zeroes/solution.java
@@ -1,17 +1,19 @@
-        int index = 0;
-        
-        for (int i =0;i<nums.length;i++){
-            if(nums[i] != 0){
-        }
-                temp[index] = nums[i];
-            }
-                index++;
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int[] temp = new int [nums.length];
 
-        for (int i=0;i<nums.length;i++){
-            nums[i]=temp[i];
-        }
-    }
+        int index = 0;
+        
+        for (int i =0;i<nums.length;i++){
+            if(nums[i] != 0){
+                temp[index] = nums[i];
+                index++;
+            }
+        }
 
-        int[] temp = new int [nums.length];
+        for (int i=0;i<nums.length;i++){
+            nums[i]=temp[i];
+        }
 
+    }
 }
