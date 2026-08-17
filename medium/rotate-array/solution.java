b979aa7b77d7895da9c4428a03c3635620e6fb53
Aditya Rai <145740878+Adityaraitechguy@users.noreply.github.com>
2026-08-17 22:19:06 +0530

    ✅ Rotate Array | Medium | Java | Runtime: 0 ms | Memory: 42.4 MB | 2026-08-17

--- medium/rotate-array/solution.java
+++ medium/rotate-array/solution.java
@@ -1,19 +1,20 @@
-import java.util.*;
-
 class Solution {
     public void rotate(int[] nums, int k) {
         int n = nums.length;
         k = k % n;
-        Deque<Integer> deque = new ArrayDeque<>();
-        for (int num : nums) deque.add(num);
+        int count = 0;
         
-        for (int i = 0; i < k; i++) {
-            deque.addFirst(deque.removeLast());
-        }
-        
-        int idx = 0;
-        for (int num : deque) {
-            nums[idx++] = num;
+        for (int start = 0; count < n; start++) {
+            int current = start;
+            int prev = nums[start];
+            do {
+                int next = (current + k) % n;
+                int temp = nums[next];
+                nums[next] = prev;
+                prev = temp;
+                current = next;
+                count++;
+            } while (start != current);
         }
     }
 }
