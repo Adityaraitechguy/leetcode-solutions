c9ca8f72f27c13992d4e59f1448b1e71b9edf7bf
Aditya Rai <145740878+Adityaraitechguy@users.noreply.github.com>
2026-08-18 10:55:14 +0530

    ✅ Palindrome Number | Easy | Java | Runtime: 0 ms | Memory: 41.9 MB | 2026-08-18

---
+++ easy/0009-palindrome-number/solution.java
@@ -0,0 +1,16 @@
+            int lastDigit = x % 10;
+            x /= 10;
+            
+            rev = rev * 10 + lastDigit;
+            
+        while (x != 0) {
+        
+        int rev = 0;
+    public boolean isPalindrome(int x) {
+        }
+        if ( x == rev){
+            return true;
+        }
+    }
+        return false;
+}
