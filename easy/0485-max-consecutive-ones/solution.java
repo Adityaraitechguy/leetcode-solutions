3232a76eba0dbd510f143a4f53d3cc7cb86bf4f8
Aditya Rai <145740878+Adityaraitechguy@users.noreply.github.com>
2026-08-18 16:11:46 +0530

    ✅ Max Consecutive Ones | Easy | Java | Runtime: 0 ms | Memory: 42.5 MB | 2026-08-18

--- easy/0485-max-consecutive-ones/solution.java
+++ easy/0485-max-consecutive-ones/solution.java
@@ -1,13 +1,16 @@
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
         int count =0;
-        for (int i=1;i<nums.length;i++){
-            if (nums[i-1]==nums[i]){
+        for (int i=0;i<nums.length;i++){
+            if (nums[i]==1){
                 count++;
             }
         }
         return maxi;
-    }
         int maxi =0;
         Math.max(maxi, count);
+            else{
+                count =0;
+            }
+    }
 }
