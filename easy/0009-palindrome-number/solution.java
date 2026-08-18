0ee03dc0d484549a5b6ef1ebb8de0dc31cb0a56b
Aditya Rai <145740878+Adityaraitechguy@users.noreply.github.com>
2026-08-18 11:34:05 +0530

    ✅ Palindrome Number | Easy | Java | Runtime: 0 ms | Memory: 41.9 MB | 2026-08-18

--- easy/0009-palindrome-number/solution.java
+++ easy/0009-palindrome-number/solution.java
@@ -5,8 +5,10 @@ class Solution {
             if (str.charAt(i)!=str.charAt(str.length()-1-i)){
                 
             }
-            return false;
+            
         }
         return true;
     }
+        
+                return false;
 }
