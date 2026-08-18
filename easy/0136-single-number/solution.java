c77293b710a21aa89f5c4d48df50a0b13e290839
Aditya Rai <145740878+Adityaraitechguy@users.noreply.github.com>
2026-08-18 16:48:05 +0530

    ✅ Single Number | Easy | Java | Runtime: 1 ms (99.94%) | Memory: 46.6 MB | 2026-08-18

--- easy/0136-single-number/solution.java
+++ easy/0136-single-number/solution.java
@@ -1,17 +1,11 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        for (int i =0;i<nums.length;i++){
-            int count=0;
-            int num = nums[i];
-            for (int j =0; j<nums.length;j++){
-                if(nums[i]==nums[j]){
-                    count++;
-                }
-                
-            }
-            if (count==1) return num;
+        int result=0;
+        for(int i=0;i<nums.length;i++){
+            result = result^nums[i] ;
         }
-        return -1;
+
+        return result;
         
     }
             
