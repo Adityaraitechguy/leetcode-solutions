f311c8ba4f7d594cd5508697e1af0c6f9efd6bfe
Aditya Rai <145740878+Adityaraitechguy@users.noreply.github.com>
2026-08-18 10:20:03 +0530

    ✅ Reverse Integer | Medium | Java | Runtime: 0 ms | Memory: 42.3 MB | 2026-08-18

--- medium/0007-reverse-integer/solution.java
+++ medium/0007-reverse-integer/solution.java
@@ -1,18 +1,17 @@
         while (x != 0) {
             int lastDigit = x % 10;
             x /= 10;
-            
-            // Check for overflow before multiplying by 10 and adding lastDigit
             if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && lastDigit > 7)) {
-                return 0; // Overflow for positive numbers
+                return 0; 
             }
             if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && lastDigit < -8)) {
-                return 0; // Underflow for negative numbers
+                return 0; 
             }
-            
             rev = rev * 10 + lastDigit;
         }
         
+        
         return rev;
+
     }
 }
