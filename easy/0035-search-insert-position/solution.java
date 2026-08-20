18c80b0eee5a543aa12ca162c0a2a1b38a76b0f2
Aditya Rai <145740878+Adityaraitechguy@users.noreply.github.com>
2026-08-21 00:32:43 +0530

    ✅ Search Insert Position | Easy | Java | Runtime: 0 ms | Memory: 42.6 MB | 2026-08-20

--- easy/0035-search-insert-position/solution.java
+++ easy/0035-search-insert-position/solution.java
@@ -1,13 +1,16 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
         int n = nums.length;
-        for (int i =0;i<n;i++){
+        for (int i =1;i<n;i++){
             if (nums[i]==target){
                 return i;
             }
         }
         
         
-    }
         return 1;
+            else if (target>nums[i-1]&&target<nums[i]){
+                return i;
+            }
+    }
 }
